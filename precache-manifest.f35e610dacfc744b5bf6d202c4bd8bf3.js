685e3f7049d17e79a21cc80f745cacfed04ed0b0
nguyenthanh <ncthanhla@gmail.com>
2020-10-20 10:01:01 +0700

    Updates

---
+++ precache-manifest.f35e610dacfc744b5bf6d202c4bd8bf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "093fd8b034daa331e36b8586d3227b68",
+    "url": "/cart-redux/index.html"
+  },
+  {
+    "revision": "18dfdd0b9001262b93e4",
+    "url": "/cart-redux/static/js/2.b7a6ccde.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/cart-redux/static/js/2.b7a6ccde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e927830ddec4881d32e",
+    "url": "/cart-redux/static/js/main.dc7a76e0.chunk.js"
+  },
+  {
+    "revision": "3ac7bd143e8ab7476cbb",
+    "url": "/cart-redux/static/js/runtime-main.2f565021.js"
+  }
+]);
